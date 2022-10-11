b9ce2f51787f41b72404c4fcaa12f314f800504f
argprog <alvarezlaura1984@gmail.com>
2022-10-10 21:39:55 -0300

    componentes

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { IdiomasComponent } from './componentes/idiomas/idiomas.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProjectsComponent } from './componentes/projects/projects.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercadeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    IdiomasComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
